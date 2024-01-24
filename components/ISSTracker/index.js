f2e8fac1bbbe4fc8032ed4e763a75ee2e053d898
Merle Makoben <merle.makoben@neuefische.de>
2024-01-24 11:25:37 +0100

    Add solution

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,36 +1,47 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
-export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
-
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
   }
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
+  return res.json();
+};
+export default function ISSTracker() {
+  const {
+    data: coords,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR(URL, fetcher, { refreshInterval: 5000 });
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+  // Alternative Solution
+  // function useCoords() {
+  //   const { data, error, isLoading } = useSWR(URL, fetcher, {
+  //     refreshInterval: 5000,
+  //   });
+
+  //   return {
+  //     coords: data,
+  //     isLoading,
+  //     error,
+  //   };
+  // }
+
+  // const { coords, isLoading, error } = useCoords();
+
+  if (isLoading || !coords) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong...</p>;
 
   return (
     <main>
@@ -38,7 +49,7 @@ export default function ISSTracker() {
       <Controls
         longitude={coords.longitude}
         latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        onRefresh={() => mutate()}
       />
     </main>
   );
